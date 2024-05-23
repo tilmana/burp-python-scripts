822f1f9df4bb5997c705ea964989ea61f19fd906
Arthur <59236083+tilmana@users.noreply.github.com>
2024-05-23 11:41:29 -0400

    Update aspnet_tracker.py
    Fixed lack of URL encoding causing "desync" between actual session tracking values on server-side

--- aspnet_tracker.py
+++ aspnet_tracker.py
@@ -1,5 +1,6 @@
 from pyscripterer import BaseScript as Script
 import re
+import urllib.parse
 
 args = [extender, callbacks, helpers, toolFlag, messageIsRequest, messageInfo, macroItems]
 header_names = ["Cookie", "Authorization"]
@@ -27,9 +28,9 @@ if script.is_in_scope() and (callbacks.getToolName(toolFlag) == "Extensions"):
       viewstate = r'(__VIEWSTATE=)[^&]+'
       viewstategenerator = r'(__VIEWSTATEGENERATOR=)[^&]+'
       eventvalidation = r'(__EVENTVALIDATION=)[^&]+'
-      body = re.sub(viewstate, "__VIEWSTATE=" + state['viewstate'], body)
-      body = re.sub(viewstategenerator, "__VIEWSTATEGENERATOR=" + state['viewstategenerator'], body)
-      body = re.sub(eventvalidation, "__EVENTVALIDATION=" + state['eventvalidation'], body)
+      body = re.sub(viewstate, urllib.parse.quote("__VIEWSTATE=" + state['viewstate']), body)
+      body = re.sub(viewstategenerator, urllib.parse.quote("__VIEWSTATEGENERATOR=" + state['viewstategenerator']), body)
+      body = re.sub(eventvalidation, urllib.parse.quote("__EVENTVALIDATION=" + state['eventvalidation']), body)
     newreq = helpers.buildHttpMessage(headers, body)
     messageInfo.setRequest(newreq)
   else:
